86bdcc571e45412547cc1d60d4431f94a1243a08
Arbab-Memon <71924928+Arbab-Memon@users.noreply.github.com>
2020-10-05 00:00:12 +0500

    Update New.py

--- New.py
+++ New.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python2
+#coding=utf-8
+#The Credit For This Code Goes To Arbab Ali
+#If You Wanna Take Credits For This Code, Please Look Yourself Again...
+#Reserved2020
+##Arbab Ali Memon
 import os, sys, time, datetime, random, hashlib, re, threading, json, urllib, cookielib, getpass
 os.system('rm -rf .txt')
 for n in range(99265):
